82187d53861372a17db1d8053454ac6a343461e9
sankired <sankired@caltech.edu>
2023-06-14 04:00:47 -0700

    setup

---
+++ diff_objects.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from transformers import CLIPProcessor, CLIPModel
+
